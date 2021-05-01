c15d6c86aa61a8716e013ce05f77e4d863dc3f60
VaudKK <vaudkeith@gmail.com>
2021-05-01 21:45:33 +0300

    feat: ability to pause and restart

--- src/Game.java
+++ src/Game.java
@@ -14,11 +14,11 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     final int BAR_LENGTH = 12;
     final int DELAY = 1;
 
-    int barX = 500;
+    int barX ;
     int barY =  SCREEN_HEIGHT - UNIT_SIZE;
 
     int ballX;
-    int ballY = 400;
+    int ballY;
     int vx = 1;
     int vy = 1;
     final int BALL_WIDTH = UNIT_SIZE * 2;
@@ -27,10 +27,11 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     final int BRICK_SIZE = UNIT_SIZE * 5;
     int brickRows = 12;
     int brickColumns = 4;
-    Brick[][] bricks = new Brick[brickRows][brickColumns];
+    Brick[][] bricks;
 
     Random random;
     boolean paused = false;
+    boolean running;
 
     Timer timer;
 
@@ -46,8 +47,12 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     }
 
     private void startGame(){
-        createBricks();
+        ballY = 400;
+        barX = 500;
+        running = true;
         ballX = random.nextInt(900) + 10;
+        bricks = new Brick[brickRows][brickColumns];
+        createBricks();
         timer.start();
     }
 
@@ -67,29 +72,41 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     }
 
     private void draw(Graphics g){
-        g.setColor(Color.ORANGE);
+        if(running){
+            g.setColor(Color.ORANGE);
 
-        //Draw bar
-        g.fillRect(barX,barY,BAR_LENGTH * UNIT_SIZE,UNIT_SIZE);
+            //Draw bar
+            g.fillRect(barX,barY,BAR_LENGTH * UNIT_SIZE,UNIT_SIZE);
 
-        g.setColor(Color.RED);
+            g.setColor(Color.RED);
 
-        //Draw circle
-        g.fillOval(ballX,ballY,BALL_WIDTH,BALL_HEIGHT);
+            //Draw circle
+            g.fillOval(ballX,ballY,BALL_WIDTH,BALL_HEIGHT);
 
-        Graphics2D g2D = (Graphics2D)g;
-        //Draw bricks
-        for (int i = 0; i < brickRows; i++) {
-            for (int j = 0; j < brickColumns; j++) {
-                if(bricks[i][j].isVisible()){
-                    g2D.setColor(Color.WHITE);
-                    g2D.fillRect(bricks[i][j].getBrickX(),bricks[i][j].getBrickY(),bricks[i][j].getBrickWidth(),bricks[i][j].getBrickHeight());
-                    g2D.setStroke(new BasicStroke(3));
-                    g2D.setColor(Color.BLACK);
-                    g2D.drawRect(bricks[i][j].getBrickX(),bricks[i][j].getBrickY(),bricks[i][j].getBrickWidth(),bricks[i][j].getBrickHeight());
+            Graphics2D g2D = (Graphics2D)g;
+            //Draw bricks
+            for (int i = 0; i < brickRows; i++) {
+                for (int j = 0; j < brickColumns; j++) {
+                    if(bricks[i][j].isVisible()){
+                        g2D.setColor(Color.WHITE);
+                        g2D.fillRect(bricks[i][j].getBrickX(),bricks[i][j].getBrickY(),bricks[i][j].getBrickWidth(),bricks[i][j].getBrickHeight());
+                        g2D.setStroke(new BasicStroke(3));
+                        g2D.setColor(Color.BLACK);
+                        g2D.drawRect(bricks[i][j].getBrickX(),bricks[i][j].getBrickY(),bricks[i][j].getBrickWidth(),bricks[i][j].getBrickHeight());
+                    }
                 }
             }
+        }else{
+            gameOver(g);
+            timer.stop();
         }
+    }
+
+    private void gameOver(Graphics g){
+        Font font = new Font("Tahoma",Font.PLAIN,30);
+        g.setColor(Color.RED);
+        g.setFont(font);
+        g.drawString("Game Over Press R to restart",10,SCREEN_HEIGHT - 100);
     }
 
     private void moveBar(char direction){
@@ -108,51 +125,61 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     }
 
     private void moveBall(){
+        if(running){
+            //Check for intersection with the bar
+            if(new Rectangle(ballX,ballY,BALL_WIDTH,BALL_HEIGHT).intersects(new Rectangle(barX,barY,BAR_LENGTH * UNIT_SIZE,UNIT_SIZE))){
+                vy = -1;
+            }
 
-        //Check for intersection with the bar
-        if(new Rectangle(ballX,ballY,BALL_WIDTH,BALL_HEIGHT).intersects(new Rectangle(barX,barY,BAR_LENGTH * UNIT_SIZE,UNIT_SIZE))){
-            vy = -1;
-        }
+            //Right border
+            if((ballX + UNIT_SIZE) == SCREEN_WIDTH){
+                vx = -1;
+            }
 
-        //Right border
-        if((ballX + UNIT_SIZE) == SCREEN_WIDTH){
-            vx = -1;
-        }
+            //Top border
+            if((ballY + UNIT_SIZE) == 0){
+                vy = 1;
+            }
 
-        //Top border
-        if((ballY + UNIT_SIZE) == 0){
-            vy = 1;
-        }
+            //Left border
+            if((ballX + UNIT_SIZE) == 0){
+                vx = 1;
+            }
 
-        //Left border
-        if((ballX + UNIT_SIZE) == 0){
-            vx = 1;
-        }
+            //Check for intersections
+            V: for (int i = 0; i < brickRows; i++) {
+                for (int j = 0; j < brickColumns; j++) {
+                    if(bricks[i][j].isVisible()){
+                        Rectangle ballBounds  =  new Rectangle(ballX,ballY,BALL_WIDTH,BALL_HEIGHT);
+                        Rectangle brickBounds = new Rectangle(bricks[i][j].getBrickX(),bricks[i][j].getBrickY(),bricks[i][j].getBrickWidth(),bricks[i][j].getBrickHeight());
 
-        //Check for intersections
-        V: for (int i = 0; i < brickRows; i++) {
-            for (int j = 0; j < brickColumns; j++) {
-                if(bricks[i][j].isVisible()){
-                    Rectangle ballBounds  =  new Rectangle(ballX,ballY,BALL_WIDTH,BALL_HEIGHT);
-                    Rectangle brickBounds = new Rectangle(bricks[i][j].getBrickX(),bricks[i][j].getBrickY(),bricks[i][j].getBrickWidth(),bricks[i][j].getBrickHeight());
+                        if(ballBounds.intersects(brickBounds)){
+                            bricks[i][j].setVisible(false);
 
-                    if(ballBounds.intersects(brickBounds)){
-                        bricks[i][j].setVisible(false);
+                            if(ballX + 1 >= (bricks[i][j].getBrickX() + BRICK_SIZE) || ballX - 1 <= bricks[i][j].getBrickX()){
+                                vx *= -1;
+                            }else{
+                                vy *= -1;
+                            }
 
-                        if(ballX + 1 >= (bricks[i][j].getBrickX() + BRICK_SIZE) || ballX - 1 <= bricks[i][j].getBrickX()){
-                            vx *= -1;
-                        }else{
-                            vy *= -1;
+                            break V;
                         }
-
-                        break V;
                     }
                 }
             }
-        }
 
-        ballX += vx;
-        ballY += vy;
+            if(ballY > SCREEN_HEIGHT){
+                running = false;
+                return;
+            }
+
+            ballX += vx;
+            ballY += vy;
+        }
+    }
+
+    private void restart(){
+       startGame();
     }
 
     @Override
@@ -164,17 +191,28 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()){
             case KeyEvent.VK_RIGHT:
-                moveBar('R');
+                if(running){
+                    moveBar('R');
+                }
                 break;
             case KeyEvent.VK_LEFT:
-                moveBar('L');
+               if(running){
+                   moveBar('L');
+               }
                 break;
             case KeyEvent.VK_ENTER:
-                paused = !paused;
-                if(paused){
-                    timer.stop();
-                }else{
-                    timer.start();
+                if (running){
+                    paused = !paused;
+                    if(paused){
+                        timer.stop();
+                    }else{
+                        timer.start();
+                    }
+                }
+                break;
+            case KeyEvent.VK_R:
+                if(!running){
+                    restart();
                 }
                 break;
         }
